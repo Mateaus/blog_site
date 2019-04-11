305bafbd18c2a260589da77d8e162fb36994357b
Joseph C <35089063+Mateaus@users.noreply.github.com>
2019-04-11 12:42:58 -0500

    Add files via upload

--- BlogProject/school_project/users/views.py
+++ BlogProject/school_project/users/views.py
@@ -6,6 +6,7 @@ from django.views.generic import DetailView
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.models import User
 from blog.models import Post
+from django.db import connection
 
 # Create your views here.
 def register(request):
@@ -27,8 +28,22 @@ class ProfileDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'User'
+        user_id = self.kwargs['pk']
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM blog_post WHERE author_id=%s", [user_id])
+        '''
+        The above execution is the equivalent of the django ORM execution below
         context['userposts'] = Post.objects.filter(author=self.kwargs['pk']).values()
+        '''
+        context['userposts'] = dictfetchall(cursor)
         return context
+
+def dictfetchall(cursor):  
+    desc = cursor.description 
+    return [
+            dict(zip([col[0] for col in desc], row)) 
+            for row in cursor.fetchall() 
+    ]
 
 
 @login_required
